bd26f64884f4035c9b8fea7351ca389b4dd573a3
Kevin Chen <kevinkchen1@gmail.com>
2023-11-06 15:51:57 -0800

    changes

--- TestListExamples.java
+++ TestListExamples.java
@@ -4,18 +4,31 @@ import java.util.Arrays;
 import java.util.List;
 
 class IsMoon implements StringChecker {
+  @Test
   public boolean checkString(String s) {
     return s.equalsIgnoreCase("moon");
   }
 }
 
 public class TestListExamples {
-  @Test(timeout = 500)
+
+  @Test
+  public void testFilter() {
+    List<String> input = Arrays.asList("moon", "sun", "star");
+    IsMoon test = new IsMoon();
+    List<String> filtered = ListExamples.filter(input, test);
+    List<String> expected = Arrays.asList("moon");
+    assertEquals(expected, filtered);
+  }
+
+  @Test
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
-  }
+  } 
+
+  
 }
--- grade.sh
+++ grade.sh
@@ -1,4 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+set -e
+
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,9 +10,19 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
+echo "test"
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+if [[! -f student-submission/ListExamples.java]]
+then
+echo "Wrong file type submitted"
+exit 1
+fi
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+
+
