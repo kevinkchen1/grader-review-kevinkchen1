f04430ca9e6ae4ba3b489764b546ca2163761ccf
Kevin Chen <kevinkchen1@gmail.com>
2023-11-27 14:47:05 -0800

    edited grading script

--- grade.sh
+++ grade.sh
@@ -16,13 +16,13 @@ echo "test"
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
-if [[! -f student-submission/ListExamples.java]]
+if [[ ! -f student-submission/ListExamples.java ]]
 then
 echo "Wrong file type submitted"
 exit 1
 fi
 
 javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
-java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > grade_output.txt
 
 
