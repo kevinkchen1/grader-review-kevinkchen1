27513d6539531d45499b4be8d90846ecf3cf0c72
Kevin Chen <kevinkchen1@gmail.com>
2023-11-27 14:48:50 -0800

    editing files
    changes

--- TestListExamples.java
+++ TestListExamples.java
@@ -22,13 +22,32 @@ public class TestListExamples {
   }
 
   @Test
+  public void testFilter2() {
+    List<String> input = Arrays.asList();
+    IsMoon test = new IsMoon();
+    List<String> filtered = ListExamples.filter(input, test);
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
+
+  @Test
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
-  } 
+  }
+
+  @Test
+  public void testMergeRightEnd2() {
+    List<String> left = Arrays.asList("1", "2", "3");
+    List<String> right = Arrays.asList("1", "4");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("1", "1", "2", "3", "4");
+    assertEquals(expected, merged);
+  }
 
   
 }
